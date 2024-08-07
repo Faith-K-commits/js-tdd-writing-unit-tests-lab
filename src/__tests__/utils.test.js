4f4d14b5fe8164f40b32348171968fa9fd805035
Faith Kariuki <tcwamboi@gmail.com>
2024-08-07 11:58:38 +0300

    Complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,21 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe('isPalindrome', () => {
+    it("should return true if a word is a palindrome", () => {
+        const word = "civic";
+        const isTrue = isPalindrome(word);
+        expect(isTrue).toEqual(true);
+    });
+
+    it("should return false if a word is not a palindrome", () => {
+        const word = "hello";
+        const isTrue = isPalindrome(word);
+        expect(isTrue).toEqual(false);
+    });
+
+    it("should return false for an empty string", () => {
+        const word = "";
+        const isTrue = isPalindrome(word);
+        expect(isTrue).toEqual(false);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
-// Your code here
+export function isPalindrome(word) {
+    if (word === "") {
+        return false;
+    }
+    let reversedWord = "";
+    for(let i = word.length - 1; i >= 0; i--){
+        reversedWord += word[i];
+    }
+    return reversedWord === word;
+}
